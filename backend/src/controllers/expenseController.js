ecf34e7f71df03ee419b3217a795b9dd9ebf6614
VilarimLucas <98033137+VilarimLucas@users.noreply.github.com>
2023-10-10 20:14:50 -0300

    deletar despeza de usuário

--- backend/src/controllers/expenseController.js
+++ backend/src/controllers/expenseController.js
@@ -6,12 +6,12 @@ const UserLogin = require('../models').UserLogin;
 const add = async (req, res) => {
     const { expenseName, isFixedExpense, isVariableExpense, valueExpense } = req.body;
     const { id } = req.params; // Este é o id do Usuário
-    
+
     try {
         // Crie a despesa
         const expense = await Expense.create({ expenseName, isFixedExpense, isVariableExpense, valueExpense });
         const idExpense = expense.id;
-        
+
         // Consulte o usuário para obter o valor atual de 'costOfLiving'
         const user = await UserLogin.findByPk(id);
         const currentCostOfLiving = user.costOfLiving;
@@ -25,7 +25,7 @@ const add = async (req, res) => {
 
         // Crie o relacionamento entre o usuário e a despesa
         const expenseUser = await ExpenseUser.create({ fk_UserLogin_id: id, fk_Expense_id: idExpense });
-        
+
         res.status(200).json({ message: 'Cadastrado com sucesso', expenseUser });
     } catch (error) {
         console.error('Erro ao cadastrar despesa do usuário:', error);
@@ -36,31 +36,68 @@ const add = async (req, res) => {
 const allByUser = async (req, res) => {
     const { id } = req.params; // Este é o ID do usuário
     try {
-      // Execute uma consulta SQL para selecionar todas as despesas do usuário com base no ID do usuário
-      const userExpenses = await ExpenseUser.sequelize.query(
-        `SELECT *
+        // Execute uma consulta SQL para selecionar todas as despesas do usuário com base no ID do usuário
+        const userExpenses = await ExpenseUser.sequelize.query(
+            `SELECT *
          FROM user_expense
-         WHERE fk_UserLogin_id = :id`, 
-        {
-          replacements: { id },
-          type: ExpenseUser.sequelize.QueryTypes.SELECT,
-        }
-      );
-  
-      res.status(200).json({ userExpenses });
-    } catch (error) {
-      console.error('Erro ao buscar despesas do usuário:', error);
-      res.status(500).json({ message: 'Erro ao buscar despesas do usuário' });
-    }
-  };
-  
+         WHERE fk_UserLogin_id = :id`,
+            {
+                replacements: { id },
+                type: ExpenseUser.sequelize.QueryTypes.SELECT,
+            }
+        );
 
+        res.status(200).json({ userExpenses });
+    } catch (error) {
+        console.error('Erro ao buscar despesas do usuário:', error);
+        res.status(500).json({ message: 'Erro ao buscar despesas do usuário' });
+    }
+};
+
+
+const del = async (req, res) => {
+    const { id } = req.params; // Este é o ID da despesa do usuário que está sendo excluída
+
+    try {
+        // Consulte a despesa pelo ID para obter o valor da despesa
+        const expenseUser = await ExpenseUser.findByPk(id);
+        if (!expenseUser) {
+            return res.status(404).json({ message: 'Despesa de usuário não encontrada' });
+        }
+
+        // Pegando o valor da despeza a partir do objeto
+        const valueExpense = parseFloat((await Expense.findByPk(expenseUser.fk_Expense_id)).valueExpense);
+
+        // Consulte o usuário para obter o valor atual de 'costOfLiving'
+        const user = await UserLogin.findByPk(expenseUser.fk_UserLogin_id);
+        if (!user) {
+            return res.status(404).json({ message: 'Usuário não encontrado' });
+        }
+        const currentCostOfLiving = parseFloat(user.costOfLiving);
+
+        // Calcule a nova somatória
+        const newCostOfLiving = currentCostOfLiving - valueExpense;
+
+        // Atualize 'costOfLiving' na tabela de usuários
+        await UserLogin.update({ costOfLiving: newCostOfLiving }, { where: { id: user.id } });
+
+        // Exclua a entrada da despesa das tabelas 'Expense' e 'ExpenseUser'
+        await ExpenseUser.destroy({ where: { id: id } });
+        await Expense.destroy({ where: { id: expenseUser.fk_Expense_id } });
+
+
+        res.status(200).json({ message: 'Despesa excluída com sucesso' });
+    } catch (error) {
+        console.error('Erro ao excluir despesa:', error);
+        res.status(500).json({ message: 'Erro ao excluir despesa', error });
+    }
+};
 
 
 module.exports = {
     add,
-    allByUser
+    allByUser,
     // specific,
     // update,
-    // del
+    del
 };
--- backend/src/router.js
+++ backend/src/router.js
@@ -19,7 +19,7 @@ router.post('/user-expense/add/:id', expense.add); // adiciona Despeza e despeza
 router.get('/user-expense/all/:id', expense.allByUser); // Retorna todas as despezas do usuário
 // router.get('/expense/:id', expense.specific); // retorna um usuário baseado em seu id
 // router.put('/expense/:id', expense.update); // atualiza os dados do usuário
-// router.delete('/expense/:id', expense.del); // delete um usuário
+router.delete('/expense/:id', expense.del); // delete um usuário
 
 
 
